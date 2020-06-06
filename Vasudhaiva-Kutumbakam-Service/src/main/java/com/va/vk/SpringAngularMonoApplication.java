84f34758e2cf3f6a0087adeaef5607b37f697b5a
Gopal <barge.gopal@gmail.com>
2020-06-06 15:45:20 +0530

    initial setup

---
+++ Vasudhaiva-Kutumbakam-Service/src/main/java/com/va/vk/SpringAngularMonoApplication.java
@@ -0,0 +1,13 @@
+package com.va.vk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularMonoApplication.class, args);
+    }
+
+}
---
+++ Vasudhaiva-Kutumbakam-Service/src/test/java/com/va/vk/SpringAngularMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.va.vk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
